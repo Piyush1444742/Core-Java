4de29857acfabf342bc242891cb920e197869765
Piyush Tupat <piyusheduc.id2004@gmail.com>
2026-03-25 19:52:20 +0530

    Create Java_Datatypes.java
    Added a Java class demonstrating various data types including integer, floating point, character, boolean, and string.

---
+++ Java Basics/Java_Datatypes.java
@@ -0,0 +1,39 @@
+public class Java_Datatypes {
+
+        public static void main(String[] args) {
+
+            // Integer types
+            byte b = 10;
+            short s = 200;
+            int i = 1000;
+            long l = 100000L;
+
+            // Floating point types
+            float f = 10.5f;
+            double d = 20.99;
+
+            // Character type
+            char c = 'A';
+
+            // Boolean type
+            boolean isJavaFun = true;
+
+            // Non-primitive
+            String str = "Hello Java";
+
+            // Printing all values
+            System.out.println("Byte value: " + b);
+            System.out.println("Short value: " + s);
+            System.out.println("Int value: " + i);
+            System.out.println("Long value: " + l);
+
+            System.out.println("Float value: " + f);
+            System.out.println("Double value: " + d);
+
+            System.out.println("Char value: " + c);
+            System.out.println("Boolean value: " + isJavaFun);
+
+            System.out.println("String value: " + str);
+        }
+    }
+
