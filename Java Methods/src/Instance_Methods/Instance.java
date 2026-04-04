67a9d0cd491e6715090c0945857e898f3895c015
Piyush Tupat <piyusheduc.id2004@gmail.com>
2026-04-04 17:52:39 +0530

    Add files via upload

---
+++ Java Methods/src/Instance_Methods/Instance.java
@@ -0,0 +1,7 @@
+package Instance_Methods;
+
+public class Instance {
+    public void printMessage() {
+        System.out.println("Hello, Friend!");
+    }
+}
---
+++ Java Methods/src/Instance_Methods/Test.java
@@ -0,0 +1,14 @@
+package Instance_Methods;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // Create an instance of the class
+        // containing the method
+        Instance obj = new Instance();
+
+        // Calling the method
+        obj.printMessage();
+    }
+}
