f33ba9bf719075a9570040a72f291b97efc41c42
Piyush Tupat <piyusheduc.id2004@gmail.com>
2026-05-03 19:15:22 +0530

    Add files via upload

---
+++ Java Practice Problems/Even_Odd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Even_Odd {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num%2==0){
+            System.out.println("Number is Even");
+        }
+        else {
+            System.out.println("Number is odd");
+        }
+        }
+    }
