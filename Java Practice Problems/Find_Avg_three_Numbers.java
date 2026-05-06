e475305f05145456ab2d5d44810a8aaf69fd9c60
Piyush Tupat <piyusheduc.id2004@gmail.com>
2026-05-06 07:30:15 +0530

    Add files via upload

---
+++ Java Practice Problems/Find_Avg_three_Numbers.java
@@ -0,0 +1,11 @@
+//Find average of 3 numbers
+
+public class Find_Avg_three_Numbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = (a + b + c)/3;
+        System.out.println("The averge of three numbers is "+ d);
+    }
+}
